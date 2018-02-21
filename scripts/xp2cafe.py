25ca4be855a1d2ae19666d392f2c4ddce4313c34
Fabio K. Mendes <binhologia@gmail.com>
2018-02-21 11:53:07 -0500

    Fixed bugs in xp2cafe.py.

--- scripts/xp2cafe.py
+++ scripts/xp2cafe.py
@@ -7,16 +7,16 @@ import sys
 tips = sys.argv[1].split(',')
 spp_names = list()
 print_header = False
-with open(sys.argv[3]) as output_file:
-    with open(sys.argv[2]) as input_data:
+with open(sys.argv[3], 'w') as output_file:
+    with open(sys.argv[2], 'r') as input_data:
         for line in input_data:
             if line.startswith('#'):
                 sp = line.lstrip('#').rstrip()
                 spp_names.append(sp)
             else:
                 if not print_header:
-                    output_file.write('\t'.join(spp_names) + '\n')
+                    output_file.write('\t'.join([n for i,n in enumerate(spp_names) if str(i+1) in tips]) + '\n')
                     print_header = True
                 tokens = line.rstrip().split('\t')
-                output_file.write('\t'.join([t for t in tokens if str(t+1) in tips]) + '\n')
+                output_file.write('\t'.join([t for j,t in enumerate(tokens) if str(j+1) in tips]) + '\n')
 
